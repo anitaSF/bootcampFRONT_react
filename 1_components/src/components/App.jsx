dda71825336e12e3f2f11c5320e72774b2720c10
anitaSF <ana.isabel.santos@bootcamp-upgrade.com>
2024-06-15 14:05:26 +0200

    repaso clase 1 react

--- 1_components/src/components/App.jsx
+++ 1_components/src/components/App.jsx
@@ -1,14 +1,21 @@
 import './App.scss' // Para asociar una hoja de estilo a un componente se debe importar
-import Button from './Button';
+import Button from './Button'; // Para importar otro componente en la estructura html se debe importar
 import Card from './Card';
+import Form from './Form';
 
 // El código js se escribe directamente dentro de las llaves de la function NombreComponente y el código html dentro del return (): function NombreComponente() {js return (html)}
 function App() {
   const name = 'Petra';
+  const lastname = 'Gonzalez';
   const mascota1 = {
     name: 'fifi',
     age: 6,
     img: 'https://live-mascotas-sanas-duenos-felices-blogs.cphostaccess.com/_Assets/img/181129-Imagen-AlimentacionMascotas.jpg'
+  }
+  const mascota2 = {
+    name: 'coco',
+    age: 3,
+    img: 'https://okdiario.com/img/2017/08/10/agaporni.jpg'
   }
 
 
@@ -16,9 +23,9 @@ function App() {
     // Para introducir más de un elemento html en el return () se deben agrupar entre las etiquetas <> </>
     <>
       <section>
-
+        {/* Para dar clases css a los elementos html, en vez de el atributo "class" se utiliza "className" */}
         <h1 className="title">Bienvenida {name}</h1>
-        <p className="paragraph">La mascota </p>
+        <p className="paragraph">La <span className='paragraph_blue'>mascota</span> es: {mascota1.name} y tiene {mascota1.age} años</p>
 
         {/* Para utilizar un componente en otro, se llama como un elemento html. Como generalmente los componentes tienen valor dinámico, el valor se le da mediante un atributo cuyo nombre equivale a la propiedad (props) de la variable dada al componente (props.text).
         Pero el valor de la variable deberá ser definido en el componente padre, en este caso "const name = 'Petra'" */}
@@ -29,6 +36,10 @@ function App() {
         Pero la infomración, es decir variables, mostrada por el componente importado debe estar definida en la function del componente padre, en este caso el objeto "const = mascota1 = {name: x, age = 0, img = 'url'}" y cada propiedad será utilizada en el componente hijo para dar valor a un elemento html creado
         */}
         <Card data={mascota1}></Card>
+        <Card data={mascota2}></Card>
+
+{/*  */}
+        <Form nombre={name} apellido={lastname}></Form>
 
       </section>
     </>
--- 1_components/src/components/Button.jsx
+++ 1_components/src/components/Button.jsx
@@ -1,5 +1,8 @@
+import PropTypes from "prop-types"
+
 // Para utilizar un componente en otros diferentes, y con valores dinámicos (diferentes), no se le define un valor absoluto, sino una propiedad. 
-// Para ello, en el argumento de la función se define que el componente va a recibir propiedades (props) y como valor del elemento html se define la variable {props.nombreProp}
+
+// Para ello, en el argumento de la función se define que el componente va a recibir propiedades (props) y como valor del elemento html se llama a la variable que será definida en el componente padre {props.nombreProp}
 // Estas propiedades configuran un array tipo objeto, de ahí que para llamarlas dentro del elemento html se utilice la nomenclatura props.nombreProp
 
 function Button(props) {
@@ -7,6 +10,10 @@ function Button(props) {
     return (
         <button>{props.text}</button>
     )
+}
+
+Button.propTypes = {
+    text: PropTypes.string
 }
 
 export default Button // exportar componente para poder utilizarlo en otros
